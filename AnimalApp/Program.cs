f33b73748be15955574aa6ab73b4a36396835e79
Robert Johansson <Wizti@users.noreply.github.com>
2024-09-05 09:53:01 +0200

    nothing special

--- AnimalApp/Program.cs
+++ AnimalApp/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace AnimalApp
+﻿using System;
+namespace AnimalApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> list = new List<string>();
+            int[] numbers = { 1, 2, 3, 4 };
+            Object[] test = [5, 6, 7, 8];
+
+            // Print normal
+            PrintArray(numbers);
+            PrintArray(test);
+
+            // copy first 2 elements from first array to test array
+            Array.Copy(numbers, test, 2);
+
+            // Print again.
+            PrintArray(numbers);
+            PrintArray(test);
+
+        }
+
+        private static void PrintArray(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+        private static void PrintArray(Object[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
         }
     }
 }
